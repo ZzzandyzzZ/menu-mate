47c3a1f30e7cd5f2f9113718680678f7e9364e92
ZzzandyzzZ <andy_gean@hotmail.com>
2023-07-30 17:58:07 -0500

    Move MenuData to data folder

---
+++ src/data/menuData.ts
@@ -0,0 +1,52 @@
+import { DaysOfWeek, ProposerNames } from "@/types.d";
+
+export const MenuData = [
+  {
+    dishName: "CHURRASCO",
+    dayOfWeek: DaysOfWeek.lunes,
+    proposerName: ProposerNames.andy,
+    imageUrl: "https://tropicalcomidarapida.com/wp-content/uploads/resized_CHURRASCO-1-1.jpg",
+  },
+  {
+    dishName: "Sushi",
+    dayOfWeek: DaysOfWeek.martes,
+    proposerName: ProposerNames.andy,
+    imageUrl:
+      "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/0749D9BC-260D-40F4-A07F-54814C4A82B4/Derivates/A73A7793-F3EE-4B90-ABA4-1CC1A0C3E18F.jpg",
+  },
+  {
+    dishName: "Hamburguesa",
+    dayOfWeek: DaysOfWeek.miercoles,
+    proposerName: ProposerNames.franzua,
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/NCI_Visuals_Food_Hamburger.jpg/640px-NCI_Visuals_Food_Hamburger.jpg",
+  },
+  {
+    dishName: "Pizza",
+    dayOfWeek: DaysOfWeek.jueves,
+    proposerName: ProposerNames.nicolay,
+    imageUrl:
+      "https://newluxbrand.com/wp-content/uploads/2022/01/pizza-jamo%CC%81n-y-queso-Newlux-1024x1024-optimized.jpg",
+  },
+  {
+    dishName: "Pollo a la brasa",
+    dayOfWeek: DaysOfWeek.viernes,
+    proposerName: ProposerNames.danny,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0G5c2w0m1kc-nSNDEoB7m9Poqz1XhEW63I9zuJ3vwApL1yprknQjl_RzliwjqZO0vuhE&usqp=CAU",
+  },
+  {
+    dishName: "Tacos",
+    dayOfWeek: DaysOfWeek.sabado,
+    proposerName: ProposerNames.geraldo,
+    imageUrl:
+      "https://elcomercio.pe/resizer/g6Ldu9oMow8edKtfighUHjyTTn4=/1200x1200/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/324VE4IKPRBSFE7QL443A6FZBI.jpg",
+  },
+  {
+    dishName: "Caldo blanco",
+    dayOfWeek: DaysOfWeek.domingo,
+    proposerName: ProposerNames.carmen,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxycVg9vHYhP7Ly3Z91O9wNZ1pZBYXZK3VaUYcvTdXC2WQ7hfDpz745eIH0wmW6-WuCDs&usqp=CAU",
+  },
+];
