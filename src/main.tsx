badc93d238f211aef7bc0d07f7e823fb4f479715
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-06 17:42:05 -0500

    Use HashRouter instead BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import "@fontsource/roboto/700.css";
 
@@ -10,8 +10,8 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CssBaseline />
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
