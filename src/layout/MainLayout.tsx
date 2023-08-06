a48b8d03364a2bea5a01a629c13ac4173f0f40e8
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-06 15:12:45 -0500

    Improve App title

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 import { MainLayoutProps } from "@/types";
 
@@ -6,7 +6,9 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <Container component="main" sx={{ bgcolor: "#91C8E4" }}>
       {children}
-      Barra inferior
+      <Typography variant="h6" textAlign="center">
+        Menu Mate
+      </Typography>
     </Container>
   );
 }
