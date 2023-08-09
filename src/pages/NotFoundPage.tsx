b0e31ea3fc44496b4dcb7a2500f030324b5f641d
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-09 18:31:27 -0500

    Create Not found page

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+export default function NotFoundPage() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        textAlign: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        minHeight: "100vh",
+        backgroundColor: "#91C8E4",
+      }}
+    >
+      <Typography variant="h1" style={{ color: "white" }}>
+        404
+      </Typography>
+      <Typography variant="h5" style={{ color: "white" }}>
+        La página que estas buscando no existe
+      </Typography>
+    </Box>
+  );
+}
