c3a326199c76aec6dbd59e29e4304badfe20db2c
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-09 18:31:11 -0500

    Improve main layot style to center elements

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,14 +1,32 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 import { MainLayoutProps } from "@/types";
 
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
-    <Container component="main" sx={{ bgcolor: "#91C8E4" }}>
-      {children}
-      <Typography variant="h6" textAlign="center">
-        Menu Mate
-      </Typography>
+    <Container
+      sx={{
+        bgcolor: "#91C8E4",
+      }}
+    >
+      <Box
+        component="main"
+        sx={{
+          bgcolor: "#91C8E4",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {children}
+        <Box position="fixed" bottom={0} zIndex={2} bgcolor="gray" width="100%">
+          <Typography variant="h6" textAlign="center">
+            Menu Mate
+          </Typography>
+        </Box>
+      </Box>
     </Container>
   );
 }
