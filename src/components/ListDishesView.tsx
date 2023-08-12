9f8fe1ca62bbbe3e464eab7487555c591576ca2b
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-12 15:35:20 -0500

    Update ListDishesView

--- src/components/ListDishesView.tsx
+++ src/components/ListDishesView.tsx
@@ -1,13 +1,14 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import AddCircleTwoToneIcon from "@mui/icons-material/AddCircleTwoTone";
 import BuildCircleTwoToneIcon from "@mui/icons-material/BuildCircleTwoTone";
 import RemoveRedEyeTwoToneIcon from "@mui/icons-material/RemoveRedEyeTwoTone";
 
-import { DishCard } from "@/components";
-import { MenuData } from "@/data";
-import MainLayout from "@/layout/MainLayout";
+import { EditDishCard } from "./EditDishCard";
+import { InfoDishCard } from "./InfoDishCard";
 import { ListDishesViewProps } from "@/types.d";
+import { MenuData } from "@/data";
 import { RedirectButton } from "./RedirectButton";
+import MainLayout from "@/layout/MainLayout";
 
 export function ListDishesView({ title, type }: ListDishesViewProps) {
   return (
@@ -15,19 +16,23 @@ export function ListDishesView({ title, type }: ListDishesViewProps) {
       <Box display="flex" justifyContent="center" py="5px">
         <Typography variant="h4">{title}</Typography>
       </Box>
-      <Box>
-        {MenuData.map((menu) => (
-          <DishCard key={menu.dishName} {...menu} />
-        ))}
-      </Box>
+      <Stack width="100%" spacing={1}>
+        {MenuData.map((menu) =>
+          type === "list" ? (
+            <InfoDishCard key={menu.dishName} {...menu} />
+          ) : (
+            <EditDishCard key={menu.dishName} {...menu} />
+          )
+        )}
+      </Stack>
       <Box sx={{ position: "fixed", bottom: 25, right: 0 }}>
-        <RedirectButton redirect="/new-dish" Icon={AddCircleTwoToneIcon} />
+        <RedirectButton redirect="/new-dish" fontSize="40px" Icon={AddCircleTwoToneIcon} />
       </Box>
       <Box sx={{ position: "fixed", bottom: 25, right: 50 }}>
         {type === "list" ? (
-          <RedirectButton redirect="/proposals" Icon={BuildCircleTwoToneIcon} />
+          <RedirectButton redirect="/proposals" fontSize="40px" Icon={BuildCircleTwoToneIcon} />
         ) : (
-          <RedirectButton redirect="/list" Icon={RemoveRedEyeTwoToneIcon} />
+          <RedirectButton redirect="/list" fontSize="40px" Icon={RemoveRedEyeTwoToneIcon} />
         )}
       </Box>
     </MainLayout>
