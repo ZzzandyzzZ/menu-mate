3f185134b156656fb259f4df490ab21c039a1013
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-12 15:35:43 -0500

    Add props to RedirectButton

--- src/components/RedirectButton.tsx
+++ src/components/RedirectButton.tsx
@@ -3,15 +3,17 @@ import { useNavigate } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import { RedirectButtonProps } from "@/types.d";
 
-export function RedirectButton({ redirect, Icon }: RedirectButtonProps) {
+export function RedirectButton({ redirect, fontSize, disabled = false, Icon }: RedirectButtonProps) {
   const navigate = useNavigate();
 
   return (
     <IconButton
+      size="small"
       onClick={() => {
         navigate(redirect);
       }}
-      sx={{ fontSize: "50px" }}
+      disabled={disabled}
+      sx={{ fontSize, bgcolor: "rgba(255, 255, 255, 0.45)" }}
     >
       <Icon fontSize="inherit" />
     </IconButton>
