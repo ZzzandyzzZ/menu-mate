50978820510d551999f245136526b57749e86e49
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-15 13:15:40 -0500

    Add login to add new dishes to store

--- src/components/DishView.tsx
+++ src/components/DishView.tsx
@@ -1,17 +1,62 @@
-import { Box, ImageList, ImageListItem, MenuItem, TextField, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, ImageList, ImageListItem, MenuItem, TextField, Typography } from "@mui/material";
 
-import MainLayout from "@/layout/MainLayout";
 import { DaysOfWeek, DishViewProps } from "@/types.d";
+import { useDishes } from "@/hooks/useDishes";
+import MainLayout from "@/layout/MainLayout";
+import { useSession } from "@/hooks/useSession";
 
-export function DishView({ title, dishImages, dishName = "", weekday = "" }: DishViewProps) {
+export function DishView({ dishImages, type }: DishViewProps) {
+  const {
+    currentDish: { dishName, weekday },
+    clearCurrDish,
+    addDish,
+    updateDish,
+  } = useDishes();
+  const { name } = useSession();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log("ADD", type, clearCurrDish);
+    if (type == "add") clearCurrDish();
+  }, [clearCurrDish, type]);
+
+  const handleClick = () => {
+    if (type == "add") {
+      addDish({ id: crypto.randomUUID(), dishName, weekday, imageUrl: "", accepted: false, proposerName: name });
+      navigate("/list");
+    } else {
+      console.log("Actualizar");
+    }
+  };
+  console.log({ dishName, weekday });
   return (
     <MainLayout>
       <Typography variant="h5" textAlign="center" sx={{ py: 1 }}>
-        {title}
+        {type === "edit" ? "Editar Plato" : "Agregar Plato"}
       </Typography>
       <Box component="form">
-        <TextField fullWidth label="Nombre del plato" sx={{ my: 1 }} size="small" value={dishName} />
-        <TextField fullWidth label="Dia de la semana" select size="small" defaultValue={weekday}>
+        <TextField
+          fullWidth
+          label="Nombre del plato"
+          sx={{ my: 1 }}
+          size="small"
+          value={dishName}
+          onChange={(e) => {
+            updateDish({ dishName: e.target.value });
+          }}
+        />
+        <TextField
+          fullWidth
+          label="Dia de la semana"
+          select
+          size="small"
+          value={weekday}
+          onChange={(e) => {
+            updateDish({ weekday: e.target.value as DaysOfWeek });
+          }}
+        >
           {Object.values(DaysOfWeek).map((option) => {
             return (
               <MenuItem value={option} key={option}>
@@ -27,6 +72,9 @@ export function DishView({ title, dishImages, dishName = "", weekday = "" }: Dis
             </ImageListItem>
           ))}
         </ImageList>
+        <Button fullWidth variant="outlined" sx={{ mb: 3 }} onClick={handleClick}>
+          {type === "edit" ? "Editar" : "Agregar"}
+        </Button>
       </Box>
     </MainLayout>
   );
