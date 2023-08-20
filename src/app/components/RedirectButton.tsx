5eee7d6e505adbc5478048e73583af50298c03db
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-20 15:06:44 -0500

    Fix redirect button style

--- src/app/components/RedirectButton.tsx
+++ src/app/components/RedirectButton.tsx
@@ -3,16 +3,12 @@ import { IconButton } from '@mui/material'
 import { type RedirectButtonProps } from '@/types.d'
 import Link from 'next/link'
 
-export function RedirectButton ({ redirect, fontSize, disabled = false, Icon }: RedirectButtonProps): JSX.Element {
+export function RedirectButton({ redirect, fontSize, disabled = false, Icon }: RedirectButtonProps): JSX.Element {
   return (
-    <IconButton
-      size="small"
-      disabled={disabled}
-      sx={{ fontSize, bgcolor: 'rgba(255, 255, 255, 0.45)' }}
-    >
-      <Link href={redirect}>
-      <Icon fontSize="inherit" />
-      </Link>
-    </IconButton>
+    <Link href={redirect}>
+      <IconButton size="small" disabled={disabled} sx={{ fontSize, bgcolor: 'rgba(255, 255, 255, 0.45)' }}>
+        <Icon fontSize="inherit" />
+      </IconButton>
+    </Link>
   )
 }
