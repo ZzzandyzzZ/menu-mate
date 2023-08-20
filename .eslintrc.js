d73cee32783babad5dbb413edbc073bebb56cbd2
ZzzandyzzZ <andy_gean@hotmail.com>
2023-08-20 15:05:40 -0500

    Change eslint json to js

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    amd: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard-with-typescript', 'plugin:jsx-a11y/recommended', 'prettier', 'next', 'next/core-web-vitals'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  plugins: ['react'],
+  rules: {
+    'comma-dangle': ['error', 'only-multiline'],
+  },
+};
