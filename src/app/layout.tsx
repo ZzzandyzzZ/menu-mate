6241b0f1a65b3afd2378199a6704143757564a93
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-02 10:39:54 -0500

    Setup roboto font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
-import { Roboto } from 'next/font/google'
 import type { Metadata } from 'next'
 
-const roboto = Roboto({
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-  subsets: ['latin']
-})
+import { CssBaseline } from '@mui/material'
+
+import Providers from '@/providers'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 export const metadata: Metadata = {
   title: 'Menu Mate',
@@ -15,7 +17,12 @@ export const metadata: Metadata = {
 export default function RootLayout ({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body>
+        <CssBaseline />
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
