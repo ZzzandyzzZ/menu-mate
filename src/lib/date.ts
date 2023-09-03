732b85702aa00538e82a89d788e90685b3454a45
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-02 22:19:30 -0500

    Create date lib

---
+++ src/lib/date.ts
@@ -0,0 +1,29 @@
+import { WeekDays } from '@/types.d'
+
+export function getMondayDate (date: Date) {
+  const tmpDate = new Date(date)
+  const day = tmpDate.getDay()
+  const diff = tmpDate.getDate() - day + (day === 0 ? -6 : 1)
+  return new Date(tmpDate.setDate(diff))
+}
+
+export function getWeekDayFromNumber (dayNumber: number): WeekDays {
+  switch (dayNumber) {
+    case 0:
+      return WeekDays.Sunday
+    case 1:
+      return WeekDays.Monday
+    case 2:
+      return WeekDays.Tuesday
+    case 3:
+      return WeekDays.Wednesday
+    case 4:
+      return WeekDays.Thursday
+    case 5:
+      return WeekDays.Friday
+    case 6:
+      return WeekDays.Saturday
+    default:
+      throw new Error('Invalid day number')
+  }
+}
