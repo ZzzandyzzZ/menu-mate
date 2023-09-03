75323d7a99c70e4b546fd0ab9267cd24259b6dba
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-03 17:04:55 -0500

    Add WeekDays and ProposerNames enums

---
+++ src/enums.ts
@@ -0,0 +1,18 @@
+export enum WeekDays {
+  Monday = 'Lunes',
+  Tuesday = 'Martes',
+  Wednesday = 'Miércoles',
+  Thursday = 'Jueves',
+  Friday = 'Viernes',
+  Saturday = 'Sábado',
+  Sunday = 'Domingo'
+}
+
+export enum ProposerNames {
+  Andy = 'Andy',
+  Carmen = 'Carmen',
+  Danny = 'Danny',
+  Nicolay = 'Nicolay',
+  Geraldo = 'Geraldo',
+  Franzua = 'Franzua'
+}
