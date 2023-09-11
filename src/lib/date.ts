33840e791e8e881f8a5bbab00b4a25a0cb75f38c
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-10 20:31:46 -0500

    update date lib

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,28 +1,60 @@
-import { WeekDays } from '@/enums'
+import { WeekDays } from '@/types/enums'
 
 export function getMondayDate(date: Date) {
-  const tmpDate = new Date(date)
+  const tmpDate = date
   const day = tmpDate.getDay()
   const diff = tmpDate.getDate() - day + (day === 0 ? -6 : 1)
   return new Date(tmpDate.setDate(diff))
 }
 
-export function getWeekDayFromNumber(dayNumber: number): WeekDays {
+export const getDateFromWeeday = (weekday: WeekDays) => {
+  const mondayStarts = (new Date().getDay() + 6) % 7
+  console.log({ mondayStarts })
+  const diffDays = mondayStarts - getNumberFromWeekday(weekday)
+  const dateOffset = 24 * 60 * 60 * 1000 * diffDays
+  const myDate = new Date()
+  console.log(dateOffset, myDate, myDate.getTime())
+  myDate.setTime(myDate.getTime() - dateOffset)
+  return myDate
+}
+
+export const getWeekDayFromNumber = (dayNumber: number): WeekDays => {
   switch (dayNumber) {
     case 0:
-      return WeekDays.Sunday
-    case 1:
       return WeekDays.Monday
-    case 2:
+    case 1:
       return WeekDays.Tuesday
-    case 3:
+    case 2:
       return WeekDays.Wednesday
-    case 4:
+    case 3:
       return WeekDays.Thursday
-    case 5:
+    case 4:
       return WeekDays.Friday
-    case 6:
+    case 5:
       return WeekDays.Saturday
+    case 6:
+      return WeekDays.Sunday
+    default:
+      throw new Error('Invalid day number')
+  }
+}
+
+export const getNumberFromWeekday = (weekday: WeekDays): number => {
+  switch (weekday) {
+    case WeekDays.Monday:
+      return 0
+    case WeekDays.Tuesday:
+      return 1
+    case WeekDays.Wednesday:
+      return 2
+    case WeekDays.Thursday:
+      return 3
+    case WeekDays.Friday:
+      return 4
+    case WeekDays.Saturday:
+      return 5
+    case WeekDays.Sunday:
+      return 6
     default:
       throw new Error('Invalid day number')
   }
