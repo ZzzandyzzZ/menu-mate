4e3c25711febdad1d3ee665011c01db505ff5895
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-10 20:34:26 -0500

    Add barrel files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,62 @@
 'use client'
+
+import { useDishService } from '@/services/dish/use-dish-service'
+import { useDishStore } from '@/store/use-dish-store'
+import { WeekDays } from '@/types'
 import { Typography } from '@mui/material'
 
-import { postToNotionDB, updateNotionPage } from '@/actions/notion-action'
-import { getNotionMenuData } from '@/services/notion'
-import { useDishesStore } from '@/store/dishes'
+export default function Home() {
+  const dishes = useDishStore((state) => state.dishes)
+  const currDish = useDishStore((state) => state.currDish)
+  const { startDishesFetching, startDishCreation } = useDishService()
 
-export default function Home () {
-  const fetchDishes = useDishesStore(state => state.fetchDishes)
-  const dishes = useDishesStore(state => state.dishes)
   const Insertar = async () => {
-    const data = await postToNotionDB({
-      id: crypto.randomUUID() as UUID,
-      dishName: 'Churrasco',
-      imageUrl: 'asd',
-      roomId: '1',
-      accepted: false,
-      dateStr: '2023-08-02',
-      proposerName: 'Carmen'
-    })
-    console.log(data)
+    void startDishCreation({ dishName: 'Churrasco', imageUrl: 'asd', weekday: WeekDays.Sunday })
+    console.log({ currDish })
   }
+
   const Listar = async () => {
-    void fetchDishes()
-    const dishes = await getNotionMenuData()
-    console.log(dishes)
+    void startDishesFetching()
+    console.log({ dishes })
   }
+
   const ver = async () => {
     console.log(dishes)
-    void updateNotionPage({ pageId: '05ba4a6d-1315-44ee-aad2-c9a3ed3cdcf8', edditableDish: { accepted: false, dishName: 'Andy' } })
+    // void updateNotionPage({
+    //   pageId: '05ba4a6d-1315-44ee-aad2-c9a3ed3cdcf8',
+    //   edditableDish: { accepted: false, dishName: 'Andy' }
+    // })
   }
+
   return (
     <main>
       <Typography variant="h1">Hello World</Typography>
-      <button onClick={() => { void Insertar() }}>Insertar</button>
-      <button onClick={() => { void Listar() }}>Listar</button>
-      <button onClick={() => { void ver() }}>ver</button>
+      <button
+        onClick={() => {
+          void Insertar()
+        }}
+      >
+        Insertar
+      </button>
+      <button
+        onClick={() => {
+          void Listar()
+        }}
+      >
+        Listar
+      </button>
+      <button
+        onClick={() => {
+          void ver()
+        }}
+      >
+        ver
+      </button>
+      {dishes.map((dish) => (
+        <div key={dish.id}>
+          {dish.dishName}, {dish.weekday}
+        </div>
+      ))}
     </main>
   )
 }
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './dish-repository.d';
+export * from './dish-service.d';
+export * from './dish-store.d';
+export * from './dish.d';
+export * from './enums';
+export * from './global.d';
