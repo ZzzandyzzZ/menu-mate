d864a828068f69bfabd0b7562090bd360c057461
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-18 20:55:50 -0500

    Get notion id to populate dish data

--- src/repositories/notion-dish/get-all.ts
+++ src/repositories/notion-dish/get-all.ts
@@ -2,17 +2,17 @@ import { fetchDishesFromNotionDB } from '@/actions/notion'
 import { getWeekDayFromNumber } from '@/lib'
 
 import type { Dish } from '@/types'
+import type { UUID } from 'crypto'
 
 type NotionProperties = Record<string, any>
 
 export const getAll = async () => {
   const results = await fetchDishesFromNotionDB('1')
-
   return results.map((page) => {
     if (!('properties' in page)) throw Error('Not data found in Notion API')
     const { properties } = page
+    const id = page.id as UUID
     const {
-      id,
       dish_name: dishName,
       proposer_name: proposerName,
       image_url: imageUrl,
@@ -21,10 +21,10 @@ export const getAll = async () => {
     }: NotionProperties = properties
     const weekdayDate = new Date(weekday.date.start)
     const dish: Dish = {
+      id,
       accepted: accepted.checkbox,
       weekday: getWeekDayFromNumber(weekdayDate.getDay()),
-      dishName: dishName.rich_text[0].plain_text,
-      id: id.title[0].plain_text,
+      dishName: dishName.title[0].plain_text,
       imageUrl: imageUrl.rich_text[0].plain_text,
       proposerName: proposerName.select.name
     }
