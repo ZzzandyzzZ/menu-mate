5f1c79c9b1719f10d12b11cb49a7ca4614cbcd04
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-24 17:19:18 -0500

    Create logout api handler

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,22 @@
+import { COOKIE_JWT_NAME } from '@/constants'
+import { validateJwtToken } from '@/lib'
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+
+export async function GET() {
+  const JWT_KEY = process.env.JWT_KEY
+  const jwtToken = cookies().get(COOKIE_JWT_NAME)
+  if (jwtToken == null) {
+    return NextResponse.json({ error: 'Not logged in' }, { status: 401 })
+  }
+  if (JWT_KEY == null) {
+    return NextResponse.json({ error: 'Env vars not defined' }, { status: 500 })
+  }
+  try {
+    await validateJwtToken(jwtToken.value, JWT_KEY)
+  } catch (error) {
+    return NextResponse.json({ error: 'Invalid token' }, { status: 401 })
+  }
+  cookies().delete(COOKIE_JWT_NAME)
+  return NextResponse.json({ message: 'Successful logout' })
+}
