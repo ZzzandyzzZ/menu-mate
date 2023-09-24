70990827c09ff33fe028a2bcfd99918d1fbb9ee4
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-24 17:20:27 -0500

    Update AuthService

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,13 +3,26 @@ import { BaseService } from './base-service'
 
 export class AuthService extends BaseService<AuthRepository> implements IAuthService {
   startLogin = async (roomId: string, proposerName: ProposerNames, password: string) => {
+    try {
+      await this.repository.setCookieToken(proposerName, password)
+    } catch (error) {
+      return {
+        success: false,
+        data: null,
+        error: (error as Error).toString()
+      }
+    }
     this.store.setState({
       roomId,
       proposerName
     })
+    return { success: true, data: null, error: null }
   }
 
   startLogout = async () => {
-    this.store((state) => state.clear)()
+    await this.handleErrorsAsync(async () => {
+      await this.repository.clearToken()
+      this.store.getState().clear()
+    })
   }
 }
