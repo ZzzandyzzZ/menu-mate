17317f8b3c23417e4c71a9a4e80606e945426ee2
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-24 20:46:21 -0500

    Use env CREDENTIALS to login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,21 +1,21 @@
 import { NextResponse } from 'next/server'
 
 import { getJwtToken } from '@/lib'
-import { credentials } from '../local-db'
 import type { KeyProposerNames } from '@/types'
 import { COOKIE_JWT_NAME } from '@/constants'
 import { cookies } from 'next/headers'
 
 export async function POST(request: Request) {
-  const JWT_KEY = process.env.JWT_KEY
+  const { JWT_KEY, CREDENTIALS } = process.env
+  if (JWT_KEY == null || CREDENTIALS == null) {
+    return NextResponse.json({ error: 'Env vars not defined' }, { status: 500 })
+  }
   const body = await request.json()
   const username: KeyProposerNames = body.username
+  const credentials = JSON.parse(CREDENTIALS)
   const userData = credentials[username]
   if (userData.password !== body.password) {
     return NextResponse.json({ error: 'Invalid username or password' }, { status: 401 })
-  }
-  if (JWT_KEY == null) {
-    return NextResponse.json({ error: 'Env vars not defined' }, { status: 500 })
   }
   const token = await getJwtToken({ userData }, JWT_KEY)
   cookies().set(COOKIE_JWT_NAME, token, {
