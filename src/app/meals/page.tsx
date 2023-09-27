e5b817ceb5488fe12157476bcbba997cf1556246
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-27 12:29:25 -0500

    Group routes meals/ and meals/proposals

--- src/app/meals/page.tsx
+++
@@ -1,9 +0,0 @@
-import { mealService } from '@/dependencies'
-
-interface Props {
-  searchParams: { room_id: string }
-}
-export default async function MealsPage({ searchParams }: Props) {
-  const meals = await mealService.getMeals(searchParams.room_id)
-  return <>{JSON.stringify(meals)}</>
-}
