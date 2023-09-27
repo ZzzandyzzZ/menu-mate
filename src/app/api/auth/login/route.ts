eb791fc5765761a7600dd7c3226a144e3c856c8f
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-27 12:28:49 -0500

    Update jwt token data

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -4,21 +4,17 @@ import { NextResponse } from 'next/server'
 import { COOKIE_JWT_NAME } from '@/constants'
 import { getJwtToken } from '@/lib'
 
-import type { KeyProposerNames } from '@/types'
-
 export async function POST(request: Request) {
   const { JWT_KEY, CREDENTIALS } = process.env
   if (JWT_KEY == null || CREDENTIALS == null) {
     return NextResponse.json({ error: 'Env vars not defined' }, { status: 500 })
   }
-  const body = await request.json()
-  const username: KeyProposerNames = body.username
-  const credentials = JSON.parse(CREDENTIALS)
-  const userData = credentials[username]
-  if (userData.password !== body.password) {
+  const { username, password: ReqPassword, roomId } = await request.json()
+  const { password, role } = JSON.parse(CREDENTIALS)[username]
+  if (password !== ReqPassword) {
     return NextResponse.json({ error: 'Invalid username or password' }, { status: 401 })
   }
-  const token = await getJwtToken({ userData }, JWT_KEY)
+  const token = await getJwtToken({ role, username, roomId }, JWT_KEY)
   cookies().set(COOKIE_JWT_NAME, token, {
     sameSite: 'strict',
     path: '/',
