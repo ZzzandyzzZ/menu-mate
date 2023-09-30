602083620d9b9047e13113dd7c9b9e3b8401fdba
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 09:04:02 -0500

    handle loading server action

--- src/components/meal/edit-meal-card.tsx
+++ src/components/meal/edit-meal-card.tsx
@@ -1,30 +1,34 @@
 'use client'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-import { Card, CardActionArea, CircularProgress, Grid, IconButton } from '@mui/material'
 import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone'
 import LockTwoTone from '@mui/icons-material/LockTwoTone'
+import { Card, CardActionArea, CircularProgress, Grid, IconButton } from '@mui/material'
 
-import { MealCardData } from '.'
 import { mealService } from '@/dependencies'
+import { MealCardData } from '.'
 
 import type { Meal } from '@/types'
 
 export function EditMealCard({ weekday, proposerName, mealName, id, accepted }: Meal) {
   const router = useRouter()
   const [loading, setLoading] = useState(false)
+  const [isAccepted, setIsAccepted] = useState(accepted)
   const { updateMeal } = mealService
 
   const onMealCardClick = (): void => {
-    if (accepted) return
+    if (isAccepted) return
     router.push('/meals/edit/' + id)
   }
 
   const onUpdateMealClick = () => {
     setLoading(true)
-    updateMeal({ id, accepted: !accepted }).finally(() => {
-      setLoading(false)
-    })
+    updateMeal({ id, accepted: !isAccepted })
+      .then(() => {
+        setIsAccepted(!isAccepted)
+      })
+      .catch(console.log)
+      .finally(() => setLoading(false))
   }
 
   const EditMealButton = () => {
@@ -33,16 +37,20 @@ export function EditMealCard({ weekday, proposerName, mealName, id, accepted }: 
         onClick={onUpdateMealClick}
         sx={{ fontSize: '22px', bgcolor: 'rgba(255, 255, 255, 0.45)' }}
       >
-        {accepted ? <LockTwoTone fontSize="inherit" /> : <LockOpenTwoToneIcon fontSize="inherit" />}
+        {isAccepted ? (
+          <LockTwoTone fontSize="inherit" />
+        ) : (
+          <LockOpenTwoToneIcon fontSize="inherit" />
+        )}
       </IconButton>
     )
   }
 
   return (
-    <Card elevation={5} sx={{ p: 1, bgcolor: accepted ? '#C6E5B1' : '#f9c1be' }}>
+    <Card elevation={5} sx={{ p: 1, bgcolor: isAccepted ? '#C6E5B1' : '#f9c1be' }}>
       <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={9} textAlign="left">
-          <CardActionArea onClick={onMealCardClick} disabled={accepted}>
+          <CardActionArea onClick={onMealCardClick} disabled={isAccepted || loading === true}>
             <MealCardData weekday={weekday} proposerName={proposerName} mealName={mealName} />
           </CardActionArea>
         </Grid>
