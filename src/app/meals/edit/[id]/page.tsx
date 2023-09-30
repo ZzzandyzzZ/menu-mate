ede0748a88cf2c8aa29ff64a58cfb3c75520b60a
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 08:22:47 -0500

    Create meals/edit/[id] path

---
+++ src/app/meals/edit/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { MealForm, PageTitle } from '@/components'
+import { mealService } from '@/dependencies'
+import { useJwtData } from '@/hooks'
+
+interface Props {
+  params: {
+    id: string
+  }
+  searchParams?: {
+    search?: string
+  }
+}
+
+export default async function EditMealPage({ params }: Props) {
+  const jwtData = await useJwtData()
+  const meal = await mealService.getMealById(params.id, jwtData?.roomId as string)
+  return (
+    <>
+      <PageTitle title="Editar Plato" />
+      <MealForm buttonText="Editar" currMealName={meal.mealName} currWeekdate={meal.weekday} />
+    </>
+  )
+}
