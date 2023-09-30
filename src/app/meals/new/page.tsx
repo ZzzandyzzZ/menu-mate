a26c2f4df250668524b13e45501bf1bfcd447787
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 08:23:00 -0500

    Create meals/new path

--- src/app/meals/new/page.tsx
+++ src/app/meals/new/page.tsx
@@ -1,3 +1,10 @@
+import { MealForm, PageTitle } from '@/components'
+
 export default function NewMealPage() {
-  return <>NewMealPage</>
+  return (
+    <>
+      <PageTitle title="Agregar Plato" />
+      <MealForm buttonText="Agregar" />
+    </>
+  )
 }
