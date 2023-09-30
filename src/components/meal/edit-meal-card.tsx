09af8d90e4be73703749fed55e897c4e1e9361ca
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 08:36:32 -0500

    update EditMealCard component

--- src/components/meal/edit-meal-card.tsx
+++ src/components/meal/edit-meal-card.tsx
@@ -1,42 +1,53 @@
 'use client'
 import { useRouter } from 'next/navigation'
-
-import { Card, CardActionArea, Grid, IconButton } from '@mui/material'
+import { useState } from 'react'
+import { Card, CardActionArea, CircularProgress, Grid, IconButton } from '@mui/material'
 import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone'
 import LockTwoTone from '@mui/icons-material/LockTwoTone'
 
-import type { Meal } from '@/types'
 import { MealCardData } from '.'
+import { mealService } from '@/dependencies'
+
+import type { Meal } from '@/types'
 
 export function EditMealCard({ weekday, proposerName, mealName, id, accepted }: Meal) {
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const { updateMeal } = mealService
 
-  const onDishCardClick = (): void => {
+  const onMealCardClick = (): void => {
     if (accepted) return
-    router.push('/dishes/edit')
+    router.push('/meals/edit/' + id)
+  }
+
+  const onUpdateMealClick = () => {
+    setLoading(true)
+    updateMeal({ id, accepted: !accepted }).finally(() => {
+      setLoading(false)
+    })
+  }
+
+  const EditMealButton = () => {
+    return (
+      <IconButton
+        onClick={onUpdateMealClick}
+        sx={{ fontSize: '22px', bgcolor: 'rgba(255, 255, 255, 0.45)' }}
+      >
+        {accepted ? <LockTwoTone fontSize="inherit" /> : <LockOpenTwoToneIcon fontSize="inherit" />}
+      </IconButton>
+    )
   }
 
   return (
     <Card elevation={5} sx={{ p: 1, bgcolor: accepted ? '#C6E5B1' : '#f9c1be' }}>
       <Grid container justifyContent="center" alignItems="center">
         <Grid item xs={9} textAlign="left">
-          <CardActionArea onClick={onDishCardClick} disabled={accepted}>
+          <CardActionArea onClick={onMealCardClick} disabled={accepted}>
             <MealCardData weekday={weekday} proposerName={proposerName} mealName={mealName} />
           </CardActionArea>
         </Grid>
         <Grid item xs={3} textAlign="center">
-          <IconButton
-            onClick={() => {
-              // toggleAcceptedOnList(id)
-            }}
-            sx={{ fontSize: '22px', bgcolor: 'rgba(255, 255, 255, 0.45)' }}
-          >
-            {accepted ? (
-              <LockTwoTone fontSize="inherit" />
-            ) : (
-              <LockOpenTwoToneIcon fontSize="inherit" />
-            )}
-          </IconButton>
+          {loading ? <CircularProgress /> : <EditMealButton />}
         </Grid>
       </Grid>
     </Card>
