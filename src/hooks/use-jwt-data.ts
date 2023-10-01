595e81cef96c562c851c434f58cce9835c913fba
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 21:21:34 -0500

    Delet hooks

--- src/hooks/use-jwt-data.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from 'next/headers'
-
-import { COOKIE_JWT_NAME } from '@/constants'
-import { getJwtData } from '@/lib'
-
-export const useJwtData = async () => {
-  const jwtToken = cookies().get(COOKIE_JWT_NAME)
-  return await getJwtData(jwtToken?.value, process.env.JWT_KEY)
-}
--- src/hooks/useMeals.ts
+++
@@ -1,8 +0,0 @@
-import { mealService } from '@/dependencies'
-import { useJwtData } from '.'
-
-export const useMeals = async () => {
-  const jwtData = await useJwtData()
-  const meals = await mealService.getMeals(jwtData?.roomId as string)
-  return meals
-}
