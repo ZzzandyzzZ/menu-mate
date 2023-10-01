8f5e71478c50675a86bff78f028cd5a3cc7c1141
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 21:19:06 -0500

    Add store client and server initializer

--- src/app/meals/layout.tsx
+++ src/app/meals/layout.tsx
@@ -1,6 +1,10 @@
-import { Box, Toolbar } from '@mui/material'
+import { Toolbar } from '@mui/material'
+import { cookies } from 'next/headers'
 
-import { DrawerAppBar } from '@/components'
+import { DrawerAppBar, StoreInitializer } from '@/components'
+import { COOKIE_JWT_NAME } from '@/constants'
+import { getJwtData } from '@/lib'
+import { useStore } from '@/store'
 
 import type { Metadata } from 'next'
 
@@ -8,14 +12,16 @@ export const metadata: Metadata = {
   title: 'Meals list'
 }
 
-export default function MealLayout({ children }: { children: React.ReactNode }) {
+export default async function MealLayout({ children }: { children: React.ReactNode }) {
+  const jwtToken = cookies().get(COOKIE_JWT_NAME)
+  const { roomId, proposerName } = await getJwtData(jwtToken?.value, process.env.JWT_KEY)
+  useStore.setState({ roomId, proposerName })
   return (
     <>
+      <StoreInitializer proposerName={proposerName} roomId={roomId} />
       <DrawerAppBar />
       <Toolbar />
-      <Box px={2} width="100%">
-        {children}
-      </Box>
+      {children}
     </>
   )
 }
