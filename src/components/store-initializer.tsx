e39b6c453b1d647e0b6a5ea2c037ccc1836b4b7a
ZzzandyzzZ <andy_gean@hotmail.com>
2023-09-30 21:19:44 -0500

    Create client StoreInitializer

---
+++ src/components/store-initializer.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useStore } from '@/store'
+import { useRef } from 'react'
+
+import { JwtData } from '@/types'
+
+export const StoreInitializer = ({ proposerName, roomId }: JwtData) => {
+  const initialized = useRef(false)
+  if (!initialized.current) {
+    console.log('client', { roomId, proposerName })
+    useStore.setState({ proposerName, roomId })
+    initialized.current = true
+  }
+  return null
+}
