8bfaed84b0940e111f7dcfce415c23367f3a4264
ZzzandyzzZ <andy_gean@hotmail.com>
2023-10-01 19:20:24 -0500

    use imageSearchResults in CustomImageList component

--- src/components/meal/meal-form/custom-image-list.tsx
+++ src/components/meal/meal-form/custom-image-list.tsx
@@ -1,18 +1,16 @@
-import { ImageList, ImageListItem } from '@mui/material'
+import { ImageList, ImageListItem, Typography } from '@mui/material'
 
-interface DishImage {
-  img: string
-  title: string
-}
+import { useStore } from '@/store'
 
-interface Props {
-  dishImages: DishImage[]
-}
-
-export const CustomImageList = ({ dishImages }: Props) => {
-  return (
+export const CustomImageList = () => {
+  const imageSearchResults = useStore((state) => state.imageSearchResults)
+  return imageSearchResults.length === 0 ? (
+    <Typography textAlign="center" py={3}>
+      Sin resultados de imagenes
+    </Typography>
+  ) : (
     <ImageList cols={2}>
-      {dishImages.slice(0, 6).map(({ img, title }) => (
+      {imageSearchResults.slice(0, 6).map(({ img, title }) => (
         <ImageListItem key={img} sx={{ bgcolor: '#F6F4EB' }}>
           <img src={img} alt={title} />
         </ImageListItem>
