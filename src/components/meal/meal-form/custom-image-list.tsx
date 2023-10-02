ac8f0c2061281953b4676aa1190737cc48f61992
ZzzandyzzZ <andy_gean@hotmail.com>
2023-10-01 20:00:41 -0500

    Add pagination to CustomImageList

--- src/components/meal/meal-form/custom-image-list.tsx
+++ src/components/meal/meal-form/custom-image-list.tsx
@@ -1,20 +1,54 @@
-import { ImageList, ImageListItem, Typography } from '@mui/material'
+'use client'
+
+import { ImageList, ImageListItem, Pagination, Stack, Typography } from '@mui/material'
 
 import { useStore } from '@/store'
+import { useState } from 'react'
+
+const listSize = 6
 
 export const CustomImageList = () => {
   const imageSearchResults = useStore((state) => state.imageSearchResults)
-  return imageSearchResults.length === 0 ? (
-    <Typography textAlign="center" py={3}>
-      Sin resultados de imagenes
-    </Typography>
-  ) : (
-    <ImageList cols={2}>
-      {imageSearchResults.slice(0, 6).map(({ img, title }) => (
-        <ImageListItem key={img} sx={{ bgcolor: '#F6F4EB' }}>
-          <img src={img} alt={title} />
-        </ImageListItem>
-      ))}
-    </ImageList>
+  const [page, setPage] = useState(1)
+  const [srcWithErrors, setSrcWithErrors] = useState<string[]>([])
+  const firstRange = (page - 1) * listSize
+  if (imageSearchResults.length === 0) {
+    return (
+      <Typography textAlign="center" py={3}>
+        Sin resultados de imagenes
+      </Typography>
+    )
+  }
+  return (
+    <>
+      <Stack alignItems="center" pt={1}>
+        <Pagination
+          count={Math.round(imageSearchResults.length / 6)}
+          page={page}
+          onChange={(event: React.ChangeEvent<unknown>, value: number) => {
+            setPage(value)
+          }}
+          siblingCount={0}
+          variant="outlined"
+          color="standard"
+        />
+      </Stack>
+      <ImageList cols={2}>
+        {imageSearchResults.slice(firstRange, firstRange + listSize).map(({ imgSrc, title }) => {
+          if (srcWithErrors.includes(imgSrc)) return null
+          return (
+            <ImageListItem key={imgSrc} sx={{ bgcolor: '#F6F4EB' }}>
+              <img
+                src={imgSrc}
+                alt={title}
+                onError={() => {
+                  setSrcWithErrors([...srcWithErrors, imgSrc])
+                }}
+              />
+            </ImageListItem>
+          )
+        })}
+      </ImageList>
+    </>
   )
 }
