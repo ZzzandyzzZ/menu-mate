0436f65b1f8a35513af7a608196eae28d9df53d7
ZzzandyzzZ <andy_gean@hotmail.com>
2023-10-08 09:35:41 -0500

    Add env var definition

---
+++ src/environment.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    SERPAPI_KEY: string
+    NOTION_API_KEY: string
+    NOTION_DB_ID: string
+    JWT_KEY: string
+    CREDENTIALS: string
+    ROOM_IDS: string
+  }
+}
