7a34c5c385e3dde38c1e030c79b0a8cf0a3302d1
ZzzandyzzZ <andy_gean@hotmail.com>
2023-10-08 09:37:37 -0500

    create authenticateAndGetNotionClient function

---
+++ src/actions/notion/authenticate-and-get-notion-client.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { Client } from '@notionhq/client'
+import { cookies } from 'next/headers'
+
+import { JWT_KEY, NOTION_API_KEY, NOTION_DB_ID } from '@/config'
+import { COOKIE_JWT_NAME } from '@/constants'
+import { getJwtData } from '@/lib'
+
+const getNotionClient = () => {
+  if (NOTION_API_KEY == null) throw Error('NOTION_API_KEY not found')
+  if (NOTION_DB_ID == null) throw Error('NOTION_DB_ID not found')
+  return {
+    notion: new Client({
+      auth: NOTION_API_KEY
+    }),
+    databaseId: NOTION_DB_ID
+  }
+}
+
+export const authenticateAndGetNotionClient = async () => {
+  const jwtToken = cookies().get(COOKIE_JWT_NAME)
+  if (jwtToken == null) {
+    throw Error('Unauthorized')
+  }
+  const jwtData = await getJwtData(jwtToken.value, JWT_KEY)
+  const notionClient = getNotionClient()
+  return { jwtData, notionClient }
+}
