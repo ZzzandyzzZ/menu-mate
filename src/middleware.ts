a9e90623a227ead09db903cf04c7351b1cf01fb5
ZzzandyzzZ <andy_gean@hotmail.com>
2023-10-14 22:51:23 -0500

    improve middleware logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,39 +1,55 @@
 import { NextResponse, type NextRequest } from 'next/server'
 
+import { decodeJwt } from 'jose'
 import { JWT_KEY, ROOM_IDS } from './config'
 import { COOKIE_JWT_NAME } from './constants'
-import { validateJwtToken } from './lib'
+import { hasValidToken } from './lib'
 
-export async function middleware(request: NextRequest) {
-  const { pathname, searchParams } = request.nextUrl
-  const token = request.cookies.get(COOKIE_JWT_NAME)
+export async function middleware({ url, nextUrl, cookies }: NextRequest) {
+  const { pathname, searchParams, href } = nextUrl
+  const token = cookies.get(COOKIE_JWT_NAME)
   const roomId = searchParams.get('room_id')
   const availableRooms = ROOM_IDS
+  let validToken = false
+  let tokenRoomId = null
+  if (token != null) {
+    validToken = await hasValidToken(token.value, JWT_KEY)
+    try {
+      tokenRoomId = await decodeJwt(token.value).roomId
+    } catch (error) {}
+  }
   if (pathname === '/login') {
-    if (token != null) {
-      return NextResponse.redirect(new URL('/meals', request.url))
-    }
     if (roomId == null || roomId === '') {
-      return NextResponse.redirect(new URL('/?error=NeedRoom', request.url))
+      return NextResponse.redirect(new URL('/?error=NeedRoom', url))
     }
     if (availableRooms == null) {
-      return NextResponse.redirect(new URL('/?error=NotAvailableRooms', request.url))
+      return NextResponse.redirect(new URL('/?error=NotAvailableRooms', url))
     }
     if (!availableRooms.split(',').includes(roomId)) {
-      return NextResponse.redirect(new URL('/?error=InvalidRoom', request.url))
+      return NextResponse.redirect(new URL('/?error=InvalidRoom', url))
+    }
+    if (validToken) {
+      return NextResponse.redirect(new URL('/meals', url))
+    }
+    if (!validToken && token != null) {
+      const url = new URL(href)
+      url.searchParams.append('error', 'LoginAgain')
+      const response = NextResponse.redirect(url)
+      response.cookies.delete(COOKIE_JWT_NAME)
+      return response
     }
   }
   if (pathname.startsWith('/meals')) {
-    if (token == null) {
-      return NextResponse.redirect(new URL('/?error=NotLogged', request.url))
+    if (validToken) {
+      return NextResponse.next()
+    }
+    if (tokenRoomId != null) {
+      const redirUrl = new URL(new URL(`/login?room_id=${tokenRoomId}&error=ExpiredToken`, url))
+      const response = NextResponse.redirect(redirUrl)
+      response.cookies.delete(COOKIE_JWT_NAME)
+      return response
     } else {
-      try {
-        await validateJwtToken(token.value, JWT_KEY)
-      } catch (error) {
-        const response = NextResponse.redirect(new URL('/?error=InvalidToken', request.url))
-        response.cookies.delete(COOKIE_JWT_NAME)
-        return response
-      }
+      return NextResponse.redirect(new URL('/?error=NotLogged', url))
     }
   }
   return NextResponse.next()
