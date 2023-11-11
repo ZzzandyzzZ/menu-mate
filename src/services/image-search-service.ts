cee5e7180cfb7d4a2c049106c9cfb7ad218fc0ba
ZzzandyzzZ <andy_gean@hotmail.com>
2023-11-11 15:48:25 -0500

    delete store set state from ImageSearchService

--- src/services/image-search-service.ts
+++ src/services/image-search-service.ts
@@ -5,7 +5,6 @@ import type { ImageSearchService as IImageSearchService, ImageSearchRepository }
 class ImageSearchService extends BaseService<ImageSearchRepository> implements IImageSearchService {
   getByQuery = async (query: string) => {
     const data = await this.repository.getByQuery(query)
-    this.store.setState({ imageSearchResults: data })
     return data
   }
 }
